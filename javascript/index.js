d59f2dedd7a42673a2bc3daf0988b5d8a41d7ca2
noeneyra <noeneyra@gmail.com>
2023-07-25 18:39:25 -0300

    Javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,92 +1,177 @@
-alert("Hola! Bienvenidx a Munna");
+//const prendas = initProducts();
+var prendas = [];
+var prendasSeleccionadas = [];
 
-let bienvenida = prompt ("Ingresa tu Nombre");
-console.log (bienvenida)
+function initProducts(){
 
-var opcionMenu = '1';
-var opcionCategorias = '1';
-var subtotal;
-var cantidad;
-var error = 'La opcion es incorrecta';
-var zipCode;
-var envio;
+    const ele = document.getElementById('items');
 
-while(opcionCategorias != '0'){
-    opcionMenu = prompt("Hola " + bienvenida + " ¿Qué estás buscando? \n 1 - Sets \n 2 - Bombis \n 3 - Línea Shine \n 0 - Salir");
-    if(opcionMenu == '0'){
-        break;
-    }else if(opcionMenu == '1' || opcionMenu == '2' || opcionMenu == '3'){
-        do{
-            switch(opcionMenu){
-                case '1':
-                    opcionCategorias = prompt(" Elegí tu set favorito: \n 1 - Set Kylie ($4.500) \n 2 - Set Cassie ($4.700) \n 3 - Set Ashley ($4.800) \n 4 - Set Kendall ($4.200) \n 5 - Set Ginebra ($4.500)\n 6 - Set Venus ($4.700)\n 7 - Set Mónaco ($4.800) \n 8 - Set Cali ($4.900) \n 9 - Volver al menu anterior \n 0 - Salir");
-                    switch(opcionCategorias){
-                        case '1':
-                        case '5':
-                            subtotal = 4500;
-                        break;
-                        case '2':
-                        case '6':    
-                            subtotal = 4700;
-                        break;
-                        case '3':
-                        case '7':
-                            subtotal = 4800;
-                        break;
-                        case '4':
-                            subtotal = 4200;
-                        break;
-                        case '8':
-                            subtotal = 4900;
-                        break;
-                    }
-                break;
-                case '2':
-                    opcionCategorias = prompt('Estos son las bobis disponibles: \n 1 - Lisas ($500) \n 2 - Rayadas ($850) \n 9 - Volver al menu anterior \n 0 - Salir');
-                    switch(opcionCategorias){
-                        case '1':
-                            subtotal = 500;
-                            break;
-                        case '2':
-                            subtotal = 580;
-                            break;
-                    }
-                break;
-                case '3':
-                    opcionCategorias = prompt('Estos son los bodies disponibles: \n 1 - Shine vestido ($6.500) \n 2 - Shine Body ($6.000) \n 3 - Shine conjunto ($5.800) \n 9 - Volver al menu anterior \n 0 - Salir');
-                    switch(opcionCategorias){
-                        case '1':
-                            subtotal = 6500;
-                            break;
-                        case '2':
-                            subtotal = 6000;
-                            break;
-                        case '3':
-                            subtotal = 5800;
-                            break;
-                    }
-                break;   
-            }
-            if(opcionCategorias == '0' || opcionCategorias == '9'){
-                break;
-            }
-            cantidad = Number(prompt('Cuantas unidades desea comprar: '));
-            zipCode = Number(prompt('Ingrese su codigo postal:'));
-            if(zipCode >= 0 && zipCode <= 1000){
-                envio = 500;
-            }else if(zipCode > 1000 && zipCode <= 1500){
-                envio = 750;
-            }else if(zipCode > 1500 && zipCode <= 2000){
-                envio = 1000;
-            }else if(zipCode > 2000){
-                envio = 1500;
-            }
-            alert('El total del pedido es: ' + (cantidad * subtotal + envio));
-            alert('Gracias por tu compra!');
-            break;
-        }while(opcionCategorias != '9' && opcionCategorias != '0');
+    prendas = buildSets();
+    prendas = prendas.concat(buildBombis());
+    prendas = prendas.concat(buildShine());
+
+    prendas.forEach(element =>{
+        const card = '<div class="card flex-child imgZoom">' +
+         '<img src="' + element.imgSrc + '" alt="Set Ashley" style="width:100%">' + 
+         '<h1>' + element.name + '</h1> '+
+         '<p class="price"> $' + element.price +'</p>'+
+         '<p><button id="'+ element.id+ '" onClick="addCarrito()">Agregar al carrito</button></p>'+
+         '</div>';
+        const el = document.createElement('div');
+        el.classList.add('flex-container');
+        el.innerHTML = card; 
+        ele.appendChild(el);
+    });
+
+}
+
+function addCarrito(){
+
+    const element = document.getElementById('carritos');
+    let found = prendas.find(element => element.id == event.target.id);
+ 
+    prendasSeleccionadas.push(found);
+    console.log(prendasSeleccionadas);
+    let ocurrencias = prendasSeleccionadas.reduce((count,value)=>
+        (value.id == event.target.id ? count + 1 : count),0 
+    );
+    let totalCarrito = prendasSeleccionadas.reduce((count,value)=>
+        (count + value.price),0
+    );
+    let idDiv = 'delete'+found.id;
+    let idOcu = 'ocurrencias'+found.id;
+    let idPre = 'precio'+found.id;
+    
+    const innerCarrito ='<div id="'+ idDiv +'">'+
+                        '<img src="'+ found.imgSrc +'" class="imgCarrito" alt="Set Ashley">'+
+                        '<h1 class="nombre">'+ found.category + ' ' + found.name+'</h1>'+
+                        '<div class="infoCarrito">'+
+                            '<img src="/images/minus.png" onclick="removeItem(\'' + found.id + '\')" class="signoMinus" alt="minus">'+
+                            '<p class="numero" id="'+ idOcu +'">'+ 1 +'</p>'+
+                            '<img src="/images/plus.png" onclick="addItem(\'' + found.id + '\')" class="signoPlus" alt="plus">'+
+                            '<img src="/images/trash.png" onclick="borrarItem(\'' + found.id + '\')" class="trash" alt="Trash">'+
+                            '<p class="precio" id="'+idPre+'">' + found.price+'</p>'+
+                        '</div>'+
+                        '</div>';
+
+    const bodyCard = '<div id="carrito" class="carrito">'+
+                        '<div class="titCarrito">'+
+                            'Tu carrito' +
+                        '</div id="innerCarrito">'+
+                        '<div id="innerCarrito">' + innerCarrito + '</div>'
+                        '<div>'+
+                            '<hr style="width:100%", size="4", color=black>'+
+                            '<p class="total">Total:</p>'+
+                            '<p class="precioTotal">$'+totalCarrito+'</p>'+
+                            '<p class="pagar">Pagar</p>'+
+                        '</div>'+
+                    '</div>';   
+   
+    if(prendasSeleccionadas.length == 1){
+        const el = document.createElement('div');
+        el.removeAttribute('hidden');
+        el.innerHTML = bodyCard;
+        element.appendChild(el);
     }else{
-        alert(error);
+        var foundInner = document.getElementById(idDiv);
+        if(foundInner != null){
+            var vecesRepetido = document.getElementById(idOcu);
+            var precio = document.getElementById(idPre);
+            precio.innerHTML = ocurrencias * found.price;
+            vecesRepetido.innerHTML = ocurrencias;
+        }else{
+            const inner = document.getElementById('innerCarrito');
+            inner.innerHTML += innerCarrito;
+        }
     }
+}
+
+function addItem(idFound){
+    let idOcu = 'ocurrencias'+idFound;
+    let idPre = 'precio'+idFound;
+    let vecesRepetido = document.getElementById(idOcu);
+    let precio = document.getElementById(idPre);
+
+    let ocurrenciasActuales = Number(vecesRepetido.innerHTML) + 1;
+    let found = prendasSeleccionadas.find(element => element.id == idFound);
+    vecesRepetido.innerHTML = ocurrenciasActuales;
+    precio.innerHTML = found.price * ocurrenciasActuales;
+
+}
+
+function removeItem(idFound){
+    let idOcu = 'ocurrencias'+idFound;
+    let idPre = 'precio'+idFound;
+    let vecesRepetido = document.getElementById(idOcu);
+    let precio = document.getElementById(idPre);
+
+    let ocurrenciasActuales = Number(vecesRepetido.innerHTML) - 1;
+    if(ocurrenciasActuales == 0){
+        borrarItem(idFound);
+    }else{
+        let found = prendasSeleccionadas.find(element => element.id == idFound);
+        vecesRepetido.innerHTML = ocurrenciasActuales;
+        precio.innerHTML = found.price * ocurrenciasActuales;
+    }
+
+}
+
+function borrarItem(found){
+
+    prendasSeleccionadas = prendasSeleccionadas.filter(element => element.id != found);
+
+    const row = document.getElementById('delete'+found);
+    row.remove();
+    if(prendasSeleccionadas.length == 0){
+        const carrito = document.getElementById('carrito');
+        carrito.remove();
+    }
+}
+
+function terminarCompra(){
+
+}
+
+
+/* FUNCIONES HELPER */
+function buildPrenda(name,price,cat,img){
+    return {
+        id: name + cat,
+        name: name,
+        price: price,
+        category: cat,
+        imgSrc: '../images/' + img
+    };
+}
+
+function buildSets(){
+    let setsToReturn = [];
+    setsToReturn.push(buildPrenda('Kylie',4500,'Set','SetKylie.jpeg'));
+    setsToReturn.push(buildPrenda('Cassie',4700,'Set','SetCassie.jpeg'));
+    setsToReturn.push(buildPrenda('Ashley',4800,'Set','SetAshley.jpeg'));
+    setsToReturn.push(buildPrenda('Kendall',4200,'Set','SetKendall.jpeg'));
+    setsToReturn.push(buildPrenda('Ginebra',4500,'Set','SetGinebra.jpeg'));
+    setsToReturn.push(buildPrenda('Venus',4700,'Set','SetVenus.jpeg'));
+    setsToReturn.push(buildPrenda('Monaco',4800,'Set','SetMónaco.jpeg'));
+    setsToReturn.push(buildPrenda('Cali',4900,'Set','SetCali.jpeg'));
+
+    return setsToReturn;
+}
+
+function buildBombis(){
+    let bombisToReturn = [];
+    bombisToReturn.push(buildPrenda('Lisas',500,'Bombis','BombisLisas.png'));
+    bombisToReturn.push(buildPrenda('Rayadas',850,'Bombis','BombisRayadas.jpg'));
+
+    return bombisToReturn;
+}
+
+function buildShine(){
+    let shineToReturn = [];
+    shineToReturn.push(buildPrenda('Vestido',6500,'Shine','ShineVestido.png'));
+    shineToReturn.push(buildPrenda('Body',6000,'Shine','ShineBody.jpg'));
+    shineToReturn.push(buildPrenda('Conjunto',5800,'Shine','ShineConjunto.png'));
+    
+    return shineToReturn;
 }
 
